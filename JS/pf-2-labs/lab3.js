6b32b328be4fe5406fa83619fe5ad9938bf1ecd5
d4xaris <katerynaiakhnovska@gmail.com>
2026-03-20 10:46:21 +0200

    Added LRU eviction policy to memoize

--- JS/pf-2-labs/lab3.js
+++ JS/pf-2-labs/lab3.js
@@ -1,17 +1,53 @@
-function memoize(fn) {
+function memoize(fn, options = {}) {
     const cache = new Map();
+
+    const maxSize = options.maxSize || Infinity;
+    const policy = options.policy || "LRU";
+
+    function evictOne() {
+        if (cache.size === 0) return;
+
+        let keyToDelete;
+
+        if (policy === "LRU") {
+            let oldestTime = Infinity;
+
+            for (const [key, item] of cache) {
+                if (item.lastUsed < oldestTime) {
+                    oldestTime = item.lastUsed;
+                    keyToDelete = key;
+                }
+            }
+        }
+
+        if (keyToDelete !== undefined) {
+            cache.delete(keyToDelete);
+        }
+    }
 
     return function (...args) {
 
-        const key = JSON.stringify(...args);
+        const key = JSON.stringify(args);
+        const now = Date.now();
 
         if (cache.has(key)) {
-            return cache.get(key);
+            const item = cache.get(key);
+
+            item.lastUsed = now;
+
+            return item.result;
         }
 
         const result = fn(...args);
 
-        cache.set(key, result);
+        if (cache.size >= maxSize) {
+            evictOne();
+        }
+
+        cache.set(key, {
+            result: result,
+            lastUsed: now,
+        });
 
         return result;
     };
@@ -20,8 +56,13 @@ function memoize(fn) {
 const memoizedPow = memoize((a, b) => {
     console.log("calculating");
     return a ** b;
+}, {
+    maxSize: 2,
+    policy: "LRU",
 });
 
 console.log(memoizedPow(2, 3));
 console.log(memoizedPow(2, 3));
 console.log(memoizedPow(3, 3));
+console.log(memoizedPow(4, 4));
+console.log(memoizedPow(2, 3));
