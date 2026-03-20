39ab27f15799f4f1ce60ec42d546de46510e9f02
d4xaris <katerynaiakhnovska@gmail.com>
2026-03-20 13:03:48 +0200

    Added LFU eviction policy to memoize

--- JS/pf-2-labs/lab3.js
+++ JS/pf-2-labs/lab3.js
@@ -9,7 +9,16 @@ function memoize(fn, options = {}) {
 
         let keyToDelete;
 
-        if (policy === "LRU") {
+        if (policy === "LFU") {
+            let minCount = Infinity;
+
+            for (const [key, item] of cache) {
+                if (item.count < minCount) {
+                    minCount = item.count;
+                    keyToDelete = key;
+                }
+            }
+        } else if (policy === "LRU") {
             let oldestTime = Infinity;
 
             for (const [key, item] of cache) {
@@ -34,6 +43,7 @@ function memoize(fn, options = {}) {
             const item = cache.get(key);
 
             item.lastUsed = now;
+            item.count++;
 
             return item.result;
         }
@@ -47,6 +57,7 @@ function memoize(fn, options = {}) {
         cache.set(key, {
             result: result,
             lastUsed: now,
+            count: 1,
         });
 
         return result;
@@ -61,8 +72,24 @@ const memoizedPow = memoize((a, b) => {
     policy: "LRU",
 });
 
-console.log(memoizedPow(2, 3));
-console.log(memoizedPow(2, 3));
-console.log(memoizedPow(3, 3));
+console.log("LRU ==================");
+console.log(memoizedPow(2, 1000));
+console.log(memoizedPow(2, 1000));
 console.log(memoizedPow(4, 4));
-console.log(memoizedPow(2, 3));
+console.log(memoizedPow(3, 3));
+console.log(memoizedPow(2, 1000));
+
+const memoizedSum = memoize((a, b) => {
+    console.log("calculating");
+    return a + b;
+}, {
+    maxSize: 2,
+    policy: "LFU",
+});
+
+console.log("LFU ==================");
+console.log(memoizedSum(2, 1000));
+console.log(memoizedSum(2, 1000));
+console.log(memoizedSum(4, 4));
+console.log(memoizedSum(3, 3));
+console.log(memoizedSum(4, 4));
